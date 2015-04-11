3a0b3e835467bf3b514554de516059a4db1f6ddd
killermist <killermist@gmail.com>
2015-04-11 09:49:33 -0400

    Updated rsync-backup.sh
    Mentioned that target filesystem does not need snapshot abilities.  Added some spacing in configuration section so comments are more clearly associated with the variable they're talking about.

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -5,14 +5,22 @@
 # otherwise expect to enter user's SSH password/passphrase several times
 # for commands passed to the server
 # Date examples imply that "today's" backup is being done 2011-10-29
+# This script uses rsync's ability to perform incremental backups via hard links,
+# so it is not reliant on snapshot abilities of the filesystem on which the 
+# backups are being stored.  For backups that will be stored to a target that 
+# can handle its own storage and manipulation of snapshots, this method may be
+# unnecessary and could be done more cleanly other ways.
  
 # config
-## host name of ssh/rsync server
+## host name or IP of ssh/rsync server
 SYNCSERVER=torrents
+
 ## what to sync
 SYNCWHAT=/home/`whoami`  #'my' home directory
+
 ## where to sync it, as the server filestructure would recognize it
 BASEDIR=/mnt/torrent-pool/home-backups/`whoami`@`hostname`
+
 ## can be changed to taste if backups need to happen more often than daily, or whatever
 NOW=`date +%F`
 # end of config
